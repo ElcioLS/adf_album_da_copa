78f1375542c3e78ccd80c4e413f254b9326b2e7e
ElcioLS <elciols@gmail.com>
2022-10-08 16:36:31 -0300

    Alterando Cutom Dio p enviar token auth

--- lib/app/core/rest/custom_dio.dart
+++ lib/app/core/rest/custom_dio.dart
@@ -1,22 +1,28 @@
 import 'package:adf_album_da_copa/app/core/config/env/env.dart';
+import 'package:adf_album_da_copa/app/core/rest/interceptor/auth_interceptor.dart';
 import 'package:dio/dio.dart';
 import 'package:dio/native_imp.dart';
 
 class CustomDio extends DioForNative {
+  final _authInterceptor = AuthInterceptor();
+
   CustomDio()
       : super(BaseOptions(
           baseUrl: Env.i['backend_base_url'] ?? '',
           connectTimeout: 5000,
           receiveTimeout: 60000,
         )) {
-    interceptors.add(LogInterceptor(requestBody: true, responseBody: true));
+    interceptors.add(LogInterceptor(
+        requestBody: true, responseBody: true, responseHeader: true));
   }
 
   CustomDio auth() {
+    interceptors.add(_authInterceptor);
     return this;
   }
 
   CustomDio unAuth() {
+    interceptors.remove(_authInterceptor);
     return this;
   }
 }
--- lib/app/pages/home/home_page.dart
+++ lib/app/pages/home/home_page.dart
@@ -1,5 +1,6 @@
+import 'package:adf_album_da_copa/app/core/rest/custom_dio.dart';
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_getit/flutter_getit.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -13,11 +14,9 @@ class HomePage extends StatelessWidget {
       body: Center(
           child: ElevatedButton(
         onPressed: () async {
-          final sp = await SharedPreferences.getInstance();
-          sp.clear();
-          Navigator.of(context).pushNamedAndRemoveUntil('/', (route) => false);
+          context.get<CustomDio>().get('/api/me');
         },
-        child: const Text('Logout'),
+        child: const Text('Testando auth'),
       )),
     );
   }
