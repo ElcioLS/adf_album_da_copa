d67d848e928e3efaf56e41e23232c707f4db090b
ElcioLS <elciols@gmail.com>
2022-10-09 16:27:53 -0300

    Construindo Tela de GridView

--- lib/app/pages/my_stickers/my_stickers_page.dart
+++ lib/app/pages/my_stickers/my_stickers_page.dart
@@ -1,4 +1,8 @@
+import 'package:adf_album_da_copa/app/pages/my_stickers/widgets/stickers_group.dart';
+import 'package:adf_album_da_copa/app/pages/my_stickers/widgets/stickers_group_filter.dart';
 import 'package:flutter/material.dart';
+
+import 'widgets/stickers_status_filter.dart';
 
 class MyStickersPage extends StatelessWidget {
   const MyStickersPage({super.key});
@@ -9,7 +13,26 @@ class MyStickersPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Minhas Figurinhas'),
       ),
-      body: Container(),
+      body: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Column(
+              children: const [
+                StickerStatusFilter(filterSelected: ''),
+                StickerGroupFilter(),
+              ],
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) {
+                return const StickersGroup();
+              },
+              childCount: 1,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
