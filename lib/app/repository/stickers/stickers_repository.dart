04d577dd6834c3b43306b07b4a3537ae7f116750
ElcioLS <elciols@gmail.com>
2022-10-13 22:56:19 -0300

    Implementando buscas no backend - create figurinha

--- lib/app/repository/stickers/stickers_repository.dart
+++ lib/app/repository/stickers/stickers_repository.dart
@@ -1,5 +1,11 @@
 import 'package:adf_album_da_copa/app/models/groups_stickers.dart';
+import 'package:adf_album_da_copa/app/models/sticker_model.dart';
+
+import '../../models/register_sticker_model.dart';
 
 abstract class StickersRepository {
   Future<List<GroupsStickers>> getMyAlbum();
+  Future<StickerModel?> findStickerByCode(
+      String stickerCode, String stickerNumber);
+  Future<StickerModel> create(RegisterStickerModel registerStickerModel);
 }
--- lib/app/repository/stickers/stickers_repository_impl.dart
+++ lib/app/repository/stickers/stickers_repository_impl.dart
@@ -4,6 +4,8 @@ import 'dart:developer';
 import 'package:adf_album_da_copa/app/core/exceptions/repository_exception.dart';
 import 'package:adf_album_da_copa/app/core/rest/custom_dio.dart';
 import 'package:adf_album_da_copa/app/models/groups_stickers.dart';
+import 'package:adf_album_da_copa/app/models/register_sticker_model.dart';
+import 'package:adf_album_da_copa/app/models/sticker_model.dart';
 import 'package:dio/dio.dart';
 
 import './stickers_repository.dart';
@@ -25,6 +27,41 @@ class StickersRepositoryImpl implements StickersRepository {
     } on DioError catch (e, s) {
       log('Erro ao buscar album do usuario', error: e, stackTrace: s);
       throw RepositoryException(message: 'Erro ao buscar album do usuario');
+    }
+  }
+
+  @override
+  Future<StickerModel?> findStickerByCode(
+      String stickerCode, String stickerNumber) async {
+    try {
+      final result =
+          await dio.auth().get('/api/sticker-search', queryParameters: {
+        'sticker_code': stickerCode,
+        'sticker_number': stickerNumber,
+      });
+      return StickerModel.fromMap(result.data);
+    } on DioError catch (e, s) {
+      if (e.response?.statusCode == 404) {
+        return null;
+      }
+      log('Erro ao buscar figurinha', error: e, stackTrace: s);
+      throw RepositoryException(message: 'Erro ao buscar figurinha');
+    }
+  }
+
+  @override
+  Future<StickerModel> create(RegisterStickerModel registerStickerModel) async {
+    try {
+      final body = FormData.fromMap({
+        ...registerStickerModel.toMap(),
+        'sticker_image_upload': '',
+      });
+      final result = await dio.auth().post('/api/sticker', data: body);
+      return StickerModel.fromMap(result.data);
+    } on DioError catch (e, s) {
+      log('Erro ao registrar figurinha no album', error: e, stackTrace: s);
+      throw RepositoryException(
+          message: 'Erro ao registrar figurinha no album');
     }
   }
 }
